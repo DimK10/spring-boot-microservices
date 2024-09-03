ee83138d7fa33c8dd7f857501c5307204639231c
dkaitantzidis <dkaitantzidis@byte.gr>
2024-09-03 17:47:30 +0300

    S2L18 at 02:16

--- section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,16 @@
 package com.eazybytes.accounts.service.impl;
 
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.service.IAccountsService;
+import jakarta.persistence.LockModeType;
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -15,7 +20,10 @@ public class AccountsServiceImpl implements IAccountsService {
     private final CustomerRepository customerRepository;
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     public void createAccount(CustomerDto customerDto) {
-
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);
     }
 }
